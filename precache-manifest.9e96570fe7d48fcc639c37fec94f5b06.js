fad4363030008f8c6f3dac8576a659315e36cf5a
Guhan1943 <guhantamil07@gmail.com>
2024-08-28 09:52:20 +0530

    Updates

---
+++ precache-manifest.9e96570fe7d48fcc639c37fec94f5b06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27690da116f5518e239e057661fb0d74",
+    "url": "/chess-board-using-React.js/index.html"
+  },
+  {
+    "revision": "96b8cb1766f1e4a16c21",
+    "url": "/chess-board-using-React.js/static/css/main.dd3a1302.chunk.css"
+  },
+  {
+    "revision": "c0498a681c89226d5cf0",
+    "url": "/chess-board-using-React.js/static/js/2.fdeb74d3.chunk.js"
+  },
+  {
+    "revision": "96b8cb1766f1e4a16c21",
+    "url": "/chess-board-using-React.js/static/js/main.40429216.chunk.js"
+  },
+  {
+    "revision": "05a5b8ab42bb2726fcc0",
+    "url": "/chess-board-using-React.js/static/js/runtime~main.0b1207ca.js"
+  }
+]);
